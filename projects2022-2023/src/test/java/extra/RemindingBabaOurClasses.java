ff68116d47162047864f5fb036dcab47810228e9
Jessica Wang <jessicawang@Claires-iMac.attlocal.net>
2023-01-24 22:59:35 -0800

    reminding BaBa of me and Jie Jie's classes 2nd version editted

--- projects2022-2023/src/test/java/extra/RemindingBabaOurClasses.java
+++ projects2022-2023/src/test/java/extra/RemindingBabaOurClasses.java
@@ -22,7 +22,8 @@ public class RemindingBabaOurClasses {
         }
         else if(dayOfWeek.equalsIgnoreCase("monday")){
             System.out.println("7:30 PM - 蓉蓉's Chess Class w/ Coach Lou (every two weeks)");
-            System.out.println("姐姐's Clarinet Class");
+            System.out.println("3:00 PM - 姐姐's Clarinet Class");
+            System.out.println("6:00 PM - 蓉蓉's ASG tournament");
         }
         else if(dayOfWeek.equalsIgnoreCase("tuesday")){
             System.out.println("5:45 PM - 蓉蓉's MVVC Practice");
@@ -34,12 +35,12 @@ public class RemindingBabaOurClasses {
             System.out.println("7:00 PM - 蓉蓉's Chess Class w/ 李老师");
         }
         else if(dayOfWeek.equalsIgnoreCase("friday")){
-            System.out.println("N/A");
+            System.out.println("3:10 PM - 蓉蓉's ASG chess practice");
+            System.out.println("4:20 PM - 蓉蓉's Math Class");
         }
         else if(dayOfWeek.equalsIgnoreCase("saturday")){
             System.out.println("7:45 AM - 蓉蓉's MVVC Practice");
             System.out.println("3:00 PM - 蓉蓉's Java Class");
-            System.out.println("4:45 PM - 蓉蓉's Math Class");
         }
 
     }
